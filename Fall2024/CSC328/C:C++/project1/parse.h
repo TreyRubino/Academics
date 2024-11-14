927c1482602dd2c5c462be9b97e4b99c23982a4e
TreyRubino <115559658+TreyRubino@users.noreply.github.com>
2024-11-14 00:35:09 -0500

    Delete Fall2024 directory

--- Fall2024/CSC328/C:C++/project1/parse.h
+++
@@ -1,8 +0,0 @@
-/// parse.h
-#ifndef PARSE_H
-#define PARSE_H
-
-void printInstructions();
-void parser(int argc, char *argv[], double *initialCost, double *annualFuelCost, double *taxRat, int *years);
-
-#endif
